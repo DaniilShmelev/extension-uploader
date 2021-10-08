1edeacbe071c202ecdfe46cef49ce3d4a6087ab9
DaniilShmelev <v-dshmelev@microsoft.com>
2021-10-08 14:29:58 +0300

    use major version

--- danilify/index.js
+++ danilify/index.js
@@ -3,8 +3,9 @@ const fs = require('fs');
 const path = require('path');
 
 const repoPath = process.argv[2];
-const minor = process.argv[3];
-const patch = process.argv[4];
+const major = process.argv[3];
+const minor = process.argv[4];
+const patch = process.argv[5];
 
 const extensionJsonPath = path.join(repoPath, 'vsts-extension-google-play.json');
 const extensionJson = JSON.parse(fs.readFileSync(extensionJsonPath, 'utf-8'));
@@ -12,7 +13,7 @@ extensionJson.id = 'DS-google-play-2';
 extensionJson.publisher = 'v-dshmelev';
 extensionJson.name = 'Daniil Shmelevs Google Play 2';
 extensionJson.public = false;
-extensionJson.version = `3.${minor}.${patch}`;
+extensionJson.version = `${major}.${minor}.${patch}`;
 console.log(JSON.stringify(extensionJson, null, 4));
 fs.writeFileSync(extensionJsonPath, JSON.stringify(extensionJson, null, 4));
 
